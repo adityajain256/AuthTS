e177fd5e81343dedd534e6f3d47d3b35dbd02483
adityajain256 <jainaditya25522@gmail.com>
2026-05-10 23:19:31 +0530

    big mistake exposing credentials

--- src/mailer/mail.config.ts
+++ src/mailer/mail.config.ts
@@ -8,8 +8,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false,
     auth: {
-        user: "dev.aditya.developer@gmail.com",
-        pass: "nciojurevinrcxox"
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
     }
 })
 
